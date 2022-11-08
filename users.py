34c30010b50f057a830738f80e9d237210b612b3
Damia <damian.plomp@student.hu.nl>
2022-11-08 15:29:06 +0100

    Last fixes on the insert_message()

--- users.py
+++ users.py
@@ -13,37 +13,37 @@ def insert_message():
     submissionTimeStamp = now.strftime("%d/%m/%Y %H:%M:%S")
 
     station = random.choice(stations)
-    while True:
-        print("Welcome to our message system, here you can leave a message about what you found of the trip.")
-        name = input("Please tell us your name. If you wish to stay anonymous you can leave this field empty.\n>")
-        if name == '':
-            name = 'anonymous'
-        else:
-            name = name
 
-        message = input("Please tell us in under 140 words what you thought of the trip.\n>")
+    print("Welcome to our message system, here you can leave a message about what you found of the trip.")
+    name = input("Please tell us your name. If you wish to stay anonymous you can leave this field empty.\n>")
+    if name == '':
+        name = 'anonymous'
+    else:
+        name = name
 
-        if len(message) > 140:
-            print("This message was too long.")
-            return insert_message()
-        elif message == '':
-            print("Oops! You didn't tell us what you thought of your trip!")
-            return insert_message()
+    message = input("Please tell us in under 140 words what you thought of the trip.\n>")
 
-        information = [message, submissionTimeStamp, name, station]
+    if len(message) > 140:
+        print("This message was too long.")
+        return insert_message()
+    elif message == '':
+        print("Oops! You didn't tell us what you thought of your trip!")
+        return insert_message()
 
-        with open('messages.txt', 'a') as s:
-            for info in information:
-                s.write(info)
-                s.write(';')
-            s.write('\n')
+    information = [message, submissionTimeStamp, name, station]
 
-        return_message = input("Would you like to leave another message? If so, please fill in '1', if not fill in '2'\n>")
-        if insert_message() == 1:
-            return insert_message()
-        elif return_message == 2:
-            print("Thank you for your time, we hope to see you again.")
-            break
+    with open('messages.txt', 'a') as s:
+        for info in information:
+            s.write(info)
+            s.write(';')
+        s.write('\n')
+
+    return_message = input("Would you like to leave another message? If so, please fill in '1', if not fill in '2'\n>")
+    if return_message == 1:
+        return insert_message()
+    elif return_message == 2:
+        print("Thank you for your time, we hope to see you again.")
+
 
     return message, submissionTimeStamp, name, station
 
